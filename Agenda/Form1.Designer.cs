16c579d958c69bcda5b8d9a17dad9a7600792e72
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-01-30 17:01:15 -0200

    Criação de componente PButton e menu de tarefas no formulário principal.

--- Agenda/Form1.Designer.cs
+++ Agenda/Form1.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pButton3 = new Agenda.PButton();
+            this.pButton2 = new Agenda.PButton();
+            this.pButton1 = new Agenda.PButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pButton3);
+            this.groupBox1.Controls.Add(this.pButton2);
+            this.groupBox1.Controls.Add(this.pButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 295);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opções";
+            // 
+            // pButton3
+            // 
+            this.pButton3.Location = new System.Drawing.Point(32, 90);
+            this.pButton3.Name = "pButton3";
+            this.pButton3.Size = new System.Drawing.Size(106, 23);
+            this.pButton3.TabIndex = 0;
+            this.pButton3.Text = "Consulta CNPJ";
+            this.pButton3.UseVisualStyleBackColor = true;
+            // 
+            // pButton2
+            // 
+            this.pButton2.Location = new System.Drawing.Point(32, 61);
+            this.pButton2.Name = "pButton2";
+            this.pButton2.Size = new System.Drawing.Size(106, 23);
+            this.pButton2.TabIndex = 0;
+            this.pButton2.Text = "Deletar";
+            this.pButton2.UseVisualStyleBackColor = true;
+            // 
+            // pButton1
+            // 
+            this.pButton1.Location = new System.Drawing.Point(32, 32);
+            this.pButton1.Name = "pButton1";
+            this.pButton1.Size = new System.Drawing.Size(106, 23);
+            this.pButton1.TabIndex = 0;
+            this.pButton1.Text = "Nova tarefa";
+            this.pButton1.UseVisualStyleBackColor = true;
             // 
             // Inicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 350);
+            this.ClientSize = new System.Drawing.Size(676, 495);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Inicial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agenda - Adriano";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private PButton pButton3;
+        private PButton pButton2;
+        private PButton pButton1;
     }
 }
 
---
+++ Agenda/PButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public class PButton : Button
+    {
+        private int qtdeSolicitacoes = 0;
+
+        public void alteraLabel(string novaLabel)
+        {
+            this.Text = novaLabel;
+            this.qtdeSolicitacoes++;
+        }
+    }
+}
